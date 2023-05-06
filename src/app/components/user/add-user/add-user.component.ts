fffa9df2ea0cfef33e6f12e6d76e605a422232b9
DaviMartjs <davimart2005@gmail.com>
2023-05-06 14:13:16 -0300

    Poucas mudanças

--- src/app/components/user/add-user/add-user.component.ts
+++ src/app/components/user/add-user/add-user.component.ts
@@ -1,24 +1,23 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Component } from '@angular/core';
 
+const httpOptions = new HttpHeaders().append('Content-Type', 'application/json').append('Authorization', 'Bearer d0eb813d3b393c338e074a17f627d3377df3514db31869cbf2481cb0290f60b3 ').append('Access-Control-Allow-Origin', '*').append('Access-Control-Allow-Methods', 'POST')
+
+// const headers = { 'Authorization': 'Bearer d0eb813d3b393c338e074a17f627d3377df3514db31869cbf2481cb0290f60b3'};
 @Component({
   selector: 'app-add-user',
   templateUrl: './add-user.component.html',
   styleUrls: ['./add-user.component.css']
 })
 
+
 export class AddUserComponent {
-  private url = 'https://gorest.co.in/'
+
+  private url = 'https://gorest.co.in/public/v2/users'
   constructor(private http: HttpClient) { }
+
   onSubmit(addUser: any) {
-    const headers = new Headers({
-      'Content-Type': 'application/json',
-      Authorization: `Bearer d0eb813d3b393c338e074a17f627d3377df3514db31869cbf2481cb0290f60b3`,
-      'Access-Control-Allow-Origin'
-    })
-    this.http.post(this.url, addUser, { headers }).subscribe((result) => {
-      console.log(result)
-      console.warn(addUser)
-    })
+    console.log(addUser)
+    return this.http.post(this.url, addUser, { 'headers': httpOptions }).subscribe(data => console.log(data))
   }
 }
